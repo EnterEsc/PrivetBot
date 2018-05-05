96e90917d39e1a1c790066d8abe253de082c4338
EnterEsc <vicshestakov2001@gmail.com>
2018-05-05 19:50:34 +1100

    добавил изменения в бота

--- bot.py
+++ bot.py
@@ -1,24 +1,28 @@
 import requests
 
+url = "https://api.telegram.org/bot576459441:AAEDwRC3FX8SRSM7q-PMaWeuVRZL4u664tM/"
+
 def get_updates_json(request):
-	response = requests.get(request+"getUpdates")
-	return response
+    response = requests.get(request + 'getUpdates')
+    print(response)
+    return response.json()
+
 
 def last_update(data):
-	result = data['result']
-	total_update = len(result)-1
-	return result[total_update]
+    results = data['result']
+    total_updates = len(results) - 1
+    print(results[total_updates])
+    return results[total_updates]
 
-def get_chat_id(update):  
+def get_chat_id(update):
     chat_id = update['message']['chat']['id']
+    print(chat_id)
     return chat_id
 
-def send_mess(chat,text):
-	params = {'chat_id': chat, 'text': text}
-	response = requests.post(url + 'sendMessage', data=params)
-	return response
+def send_mess(chat, text):
+    params = {'chat_id': chat, 'text': text}
+    response = requests.post(url + 'sendMessage', data=params)
+    return response
 
 chat_id = get_chat_id(last_update(get_updates_json(url)))
-#send_mess(chat_id,"Priveeeeet")
-
-
+send_mess(chat_id, 'Your message goes here')
