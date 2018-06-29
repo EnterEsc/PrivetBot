be6d88e29628e92e6bbb418b20d9e48a1974e3e7
EnterEsc <vicshestakov2001@gmail.com>
2018-06-29 14:28:00 +1100

    auto commit

---
+++ script.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+git status
+git add *
+git commit -m "auto commit"
+git push origin future
---
+++ test.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import random
+aa = random.randint(0,7)
+if aa == 0:
+    print ('Привет')
+if aa == 1:
+    print ('Приветик')
+if aa == 2:
+    print ('Приветули')
+if aa == 3:
+    print ('Приветствую')
+if aa == 4:
+    print ('Добрового времени суток')
+if aa == 5:
+    print ('Салют!')
+if aa == 6:
+    print ('Ghbdtn')
+if aa == 7:
+    print ('Здравствуй')
+
